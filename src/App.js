123d0c4690d4bb2b7c73e7bfea6a7da0b4b97634
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2023-04-29 23:25:27 +0000

    Add template

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [uuid, setUuid] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+  }
+  //02e466aa-cf20-44cf-af5b-45572bfebc87
+  const validateUUID = () => {
+    // problem 
+    const matches = uuid.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i)
+    if(true) {
+      setUuid("matches")
+    } else {
+      setUuid("s")
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,18 +26,14 @@ function App() {
           GitHub Codespaces <span className="heart">♥️</span> React
         </p>
         <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
+          UUID Conversor
         </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+        <p>{uuid}</p>
+        <form onSubmit={handleSubmit}>
+          <label for="uuid">UUID</label>
+          <input type="text" id="uuid" name="uuid" />
+          <button onClick={validateUUID}>Remove Hyphen</button>
+        </form>
       </header>
     </div>
   );
