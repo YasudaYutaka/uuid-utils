9a42802bceee70c090466fb579956fcf851505bb
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2023-05-01 21:43:16 +0000

    Add  'add hyphen' method

--- src/App.js
+++ src/App.js
@@ -33,7 +33,10 @@ function App() {
 
   const addHyphen = () => {
     if(validateUUIDWithoutHyphen()) {
-      setUuidResult("valid")
+      const uuidWithHyphen = [uuid.slice(0, 8), '-', uuid.slice(8, 12), '-',
+                              uuid.slice(12, 16), '-', uuid.slice(16, 20), '-',
+                              uuid.slice(20)].join('')
+      setUuidResult(uuidWithHyphen)
     }
   }
 
