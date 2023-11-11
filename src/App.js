2403422966c96f075752e594ce5a3d77af5ede4e
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2023-11-11 00:37:03 -0300

    feat: Add components to App.js

--- src/App.js
+++ src/App.js
@@ -1,67 +1,30 @@
 import './App.css';
 import React, { useState } from "react";
 import Input from "./components/input";
-import { UilCopy } from '@iconscout/react-unicons'
+import Button from './components/button';
+import Result from './components/result';
+import { addHyphen, removeHyphen } from "./utils/uuidUtils";
+import { toUppercase, toLowercase } from "./utils/stringUtils";
 
 function App() {
   const [uuid, setUuid] = useState([]);
   const [uuidResult, setUuidResult] = useState([]);
 
-  const handleChange = (event) => {
-    setUuid(event.target.value)
-  }
+  const handleToUppercase = () => {
+    setUuidResult(toUppercase(uuid));
+  };
 
-  const validateUUID = () => {
-    const pattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i
-    let isUUID = pattern.test(uuid)
-    if(isUUID) {
-      return true
-    } else {
-      setUuidResult("Invalid UUID")
-      return false
-    }
-  }
+  const handleToLowercase = () => {
+    setUuidResult(toLowercase(uuid));
+  };
 
-  const validateUUIDWithoutHyphen = () => {
-    const pattern = /^[0-9a-f]{8}[0-9a-f]{4}[0-5][0-9a-f]{3}[089ab][0-9a-f]{3}[0-9a-f]{12}$/i
-    let isUUIDWithoutHyphen = pattern.test(uuid)
-    if(isUUIDWithoutHyphen) {
-      return true
-    } else {
-      setUuidResult("Invalid UUID Without Hyphen")
-      return false
-    }
-  }
+  const handleAddHyphen = () => {
+    setUuidResult(addHyphen(uuid));
+  };
 
-  const addHyphen = () => {
-    if(validateUUIDWithoutHyphen()) {
-      const uuidWithHyphen = [uuid.slice(0, 8), '-', uuid.slice(8, 12), '-',
-                              uuid.slice(12, 16), '-', uuid.slice(16, 20), '-',
-                              uuid.slice(20)].join('')
-      setUuidResult(uuidWithHyphen)
-    }
-  }
-
-  const removeHyphen = () => {
-    if(validateUUID()) {
-      const uuidWithoutHyphen = uuid.replaceAll('-', '')
-      setUuidResult(uuidWithoutHyphen)
-    }
-  }
-
-  const toUppercase = () => {
-    const uppercase = uuid.toUpperCase()
-    setUuidResult(uppercase)
-  }
-
-  const toLowercase = () => {
-    const lowercase = uuid.toLowerCase()
-    setUuidResult(lowercase)
-  }
-
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(uuidResult)
-  }
+  const handleRemoveHyphen = () => {
+    setUuidResult(removeHyphen(uuid));
+  };
 
   return (
     <div className="App">
@@ -72,14 +35,7 @@ function App() {
         <p className="medium">
           UUID Converter
         </p>
-        <div class="d-flex flex-row">
-          <p class="px-3">{uuidResult}</p>
-          {uuidResult.length > 0 && 
-            <div data-toggle="tooltip" data-placement="top" title="Copy">
-              <UilCopy class="result__icon" onClick={copyToClipboard} size="40" color="#61DAFB" />
-            </div>
-          }
-        </div>
+        <Result resultText={uuidResult} />
         <div class="input-group mb-3 px-5">
           <div class="input-group-prepend">
             <span class="input-group-text" id="inputGroup-sizing-default">UUID</span>
@@ -87,10 +43,10 @@ function App() {
           <Input value={uuid} onChange={setUuid} />
         </div>
         <div class="btn-group" role="group" aria-label="Basic example">
-          <button onClick={addHyphen} class="btn btn-outline-primary">Add Hyphen</button>
-          <button onClick={removeHyphen} class="btn btn-outline-primary">Remove Hyphen</button>
-          <button onClick={toUppercase} class="btn btn-outline-primary">To Uppercase</button>
-          <button onClick={toLowercase} class="btn btn-outline-primary">To Lowercase</button>
+          <Button text="Add Hyphen" onClick={handleAddHyphen} />
+          <Button text="Remove Hyphen" onClick={handleRemoveHyphen} />
+          <Button text="To Uppercase" onClick={handleToUppercase} />
+          <Button text="To Lowercase" onClick={handleToLowercase} />
         </div>
       </header>
     </div>
