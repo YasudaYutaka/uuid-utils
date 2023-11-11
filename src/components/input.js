bed42550c7815de145d02c8be40eb25b0a1caca3
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2023-11-11 00:33:01 -0300

    doc: Add comments to input.js

--- src/components/input.js
+++ src/components/input.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-// A reusable input component that takes a value and onChange function as props
+/** 
+ * A reusable input component that takes a value and onChange function as props
+ */ 
 function Input({ value, onChange }) {
   const handleChange = (event) => {
     onChange(event.target.value);
