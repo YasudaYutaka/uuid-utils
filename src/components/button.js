137c88b183d635e3e6e62a3bb3c3a7aefc5d103e
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2023-11-11 02:03:44 -0300

    doc: Update button.js comments

--- src/components/button.js
+++ src/components/button.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 /**
- * A reusable button component that takes a text and onClick function as props.
- * Renders a button element with the text and onClick function as props.
+ * A reusable button component that displays a button element with the specified text and onClick function.
+ * 
+ * @param {string} text - The text to display on the button.
+ * @param {function} onClick - A function to be called when the button is clicked.
  */
 function Button({ text, onClick }) {
   return (
