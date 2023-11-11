d59ed1aa00e091d6581608987b7ef91230b974b3
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2023-11-11 01:04:08 -0300

    feat: Adjust uuid utils to return uuid instead of returning error message

--- src/utils/uuidUtils.js
+++ src/utils/uuidUtils.js
@@ -1,13 +1,46 @@
+/**
+ * Validates a UUID without hyphens.
+ *
+ * @param {string} uuid - The UUID to validate.
+ * @returns {boolean} - True if the UUID is valid, false otherwise.
+ */
 export const validateUUIDWithoutHyphen = (uuid) => {
     const pattern = /^[0-9a-f]{8}[0-9a-f]{4}[0-5][0-9a-f]{3}[089ab][0-9a-f]{3}[0-9a-f]{12}$/i
     return pattern.test(uuid)
 }
 
+/**
+ * Validates a UUID with hyphens.
+ *
+ * @param {string} uuid - The UUID to validate.
+ * @returns {boolean} - True if the UUID is valid, false otherwise.
+ */
 export const validateUUID = (uuid) => {
     const pattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i
     return  pattern.test(uuid)
 }
 
-export const addHyphen = (uuid) => validateUUIDWithoutHyphen(uuid) ? `${uuid.slice(0, 8)}-${uuid.slice(8, 12)}-${uuid.slice(12, 16)}-${uuid.slice(16, 20)}-${uuid.slice(20)}` : "Invalid UUID: should not contain hyphens";
+/**
+ * Adds hyphens to a UUID if it doesn't have them.
+ *
+ * @param {string} uuid - The UUID to add hyphens to.
+ * @returns {string} - The UUID with hyphens if it is valid, otherwise returns an error message.
+ */
+export const addHyphen = (uuid) => 
+    validateUUID(uuid) 
+        ? uuid 
+        : validateUUIDWithoutHyphen(uuid) 
+        ? `${uuid.slice(0, 8)}-${uuid.slice(8, 12)}-${uuid.slice(12, 16)}-${uuid.slice(16, 20)}-${uuid.slice(20)}` 
+        : "Invalid UUID: should not contain hyphens";
 
-export const removeHyphen = (uuid) => (validateUUID(uuid)) ? uuid.replaceAll('-', '') : "Invalid UUID"
+/**
+ * Removes hyphens from a UUID if it has them.
+ *
+ * @param {string} uuid - The UUID to remove hyphens from.
+ * @returns {string} - The UUID without hyphens if it is valid, otherwise returns an error message.
+ */
+export const removeHyphen = (uuid) => 
+    validateUUIDWithoutHyphen(uuid) 
+        ? uuid : validateUUID(uuid) 
+        ? uuid.replaceAll('-', '') 
+        : "Invalid UUID"
