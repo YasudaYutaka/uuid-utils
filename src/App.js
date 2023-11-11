98ec114febe5b62318bcd91b147c0ee5b838c7f3
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2023-11-11 00:40:59 -0300

    feat: Remove github text

--- src/App.js
+++ src/App.js
@@ -29,9 +29,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
         <p className="medium">
           UUID Converter
         </p>
