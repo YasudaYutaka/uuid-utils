7c271d68ebf1533f2999e6b65de040a26dbe8b88
YasudaYutaka <rafael.y.yasuda@gmail.com>
2025-05-27 21:30:20 -0300

    chore: Minor fix

--- src/components/input.js
+++ src/components/input.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 /** 
  * A reusable input component that displays a UUID input field and a button to generate a new UUID.
@@ -7,16 +7,25 @@ import React from "react";
  * @param {function} onChange - A function to be called when the input field value changes.
  */ 
 function Input({value, onChange }) {
+  const [showAlert, setShowAlert] = useState(false);
+
   const handleChange = (event) => {
     onChange(event.target.value);
   };
   
   const copyToClipboard = () => {
-    navigator.clipboard.writeText(value)
+    navigator.clipboard.writeText(value);
+    setShowAlert(true);
+    setTimeout(() => setShowAlert(false), 3000);
   }
 
   return (
     <div class="mb-3 px-5 w-25 d-flex flex-column justify-content-center align-items-center">
+      {showAlert && (
+        <div className="alert alert-success w-100 text-center" role="alert">
+          UUID copiado!
+        </div>
+      )}
       <div class="input-group d-flex">
         <input type="text" class="form-control input-purple" value={value} placeholder="UUID" onChange={handleChange} />
         <div class="input-group-append">
