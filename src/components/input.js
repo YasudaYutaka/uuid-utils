ee0eb4bb7788bfe0687372b99e06bfff2314b8ee
YasudaYutaka <rafael.y.yasuda@gmail.com>
2025-05-27 21:32:59 -0300

    fix: Remove alert

--- src/components/input.js
+++ src/components/input.js
@@ -7,7 +7,6 @@ import React, { useState } from "react";
  * @param {function} onChange - A function to be called when the input field value changes.
  */ 
 function Input({value, onChange }) {
-  const [showAlert, setShowAlert] = useState(false);
 
   const handleChange = (event) => {
     onChange(event.target.value);
@@ -15,17 +14,10 @@ function Input({value, onChange }) {
   
   const copyToClipboard = () => {
     navigator.clipboard.writeText(value);
-    setShowAlert(true);
-    setTimeout(() => setShowAlert(false), 3000);
   }
 
   return (
     <div class="mb-3 px-5 w-25 d-flex flex-column justify-content-center align-items-center">
-      {showAlert && (
-        <div className="alert alert-success w-100 text-center" role="alert">
-          UUID copiado!
-        </div>
-      )}
       <div class="input-group d-flex">
         <input type="text" class="form-control input-purple" value={value} placeholder="UUID" onChange={handleChange} />
         <div class="input-group-append">
